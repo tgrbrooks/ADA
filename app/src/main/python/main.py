899cd576c50519a390efee3de5aee10715aa0ade
Tom Brooks <tom.g.r.brooks@gmail.com>
2021-01-04 21:03:02 +0000

    Update the main for deployment

--- app/src/main/python/main.py
+++ app/src/main/python/main.py
@@ -1,5 +1,6 @@
 # Local imports
 from gui.main_window import App
+import configuration as config
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
 
 # Standard imports
@@ -11,6 +12,12 @@ import sys
 if __name__ == '__main__':
 
     appctxt = ApplicationContext()
+
+    screen = appctxt.app.primaryScreen()
+    size = screen.availableGeometry()
+    config.wr = size.width()/1280.
+    config.hr = size.width()/1280.
+    #config.hr = size.height()/777.
 
     ex = App()
 
