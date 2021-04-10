Fix condition data time average

ada/data/processor.py
@@ -166,16 +166,17 @@ def time_average(xdata, ydata, window, show_err=False):
         data_x = np.array([])
         data_y = np.array([])
         while(i < len(xdata) and xdata[i] < w_i*window):
-            data_x = np.append(data_x, ydata[i])
+            data_x = np.append(data_x, xdata[i])
             data_y = np.append(data_y, ydata[i])
             i = i + 1
+        if(data_y.size == 0):
+            w_i = w_i + 1
+            continue
         mean_x = np.mean(data_x)
         mean_y = np.mean(data_y)
         std_dev = np.std(data_y, ddof=1)
         if(show_err):
             std_dev = std_dev/np.sqrt(data_y.size)
-        if(data_y.size == 0):
-            continue
         new_xdata = np.append(new_xdata, mean_x)
         new_ydata = np.append(new_ydata, mean_y)
         if(data_y.size == 1):