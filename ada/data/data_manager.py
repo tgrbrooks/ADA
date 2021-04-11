Fix condition time average with replicates

ada/data/data_manager.py
@@ -4,7 +4,7 @@ from scipy.optimize import curve_fit
 # Local includes
 from ada.data.algae_data import AlgaeData
 from ada.data.data_holder import DataHolder
-from ada.data.processor import process_data, time_average, average_data
+from ada.data.processor import process_data, time_average, time_average_arrays, average_data
 from ada.data.models import get_model
 import ada.configuration as config
 from ada.logger import logger
@@ -159,13 +159,15 @@ class DataManager():
             legend_label = self.growth_data.data_files[i].get_header_info(extra_info)
         return legend_label
 
-    def get_condition_xy_data(self, i, cond_name, xvar=None, condition_average=None):
+    def get_condition_xy_data(self, i, cond_name, xvar=None, condition_average=None, std_err=False):
         if xvar is None:
             xvar = config.xvar
         if condition_average is None:
             condition_average = config.condition_average
+        if config.std_err:
+            std_err = True
 
-        for cond in self.condition_data.data_files:
+        for cond, j in enumerate(self.condition_data.data_files):
             if self.growth_data.data_files[i].reactor != cond.reactor:
                 continue
             if self.growth_data.data_files[i].sub_reactor != cond.sub_reactor:
@@ -174,19 +176,7 @@ class DataManager():
                 continue
             if self.growth_data.data_files[i].time != cond.time:
                 continue
-            xdatas = []
-            ydatas = []
-            for rep in self.condition_data.replicate_files[i]:
-                xdata = rep.get_xdata(xvar)
-                ydata = rep.get_ydata(cond_name)
-                xdatas.append(xdata)
-                ydatas.append(ydata)
-            if len(xdatas) > 1:
-                xdata, ydata, _ = average_data(xdatas, ydatas, False)
-            if condition_average != -1:
-                xdata, ydata, _ = time_average(
-                    xdata, ydata, condition_average)
-            return xdata, ydata
+            return self.get_condition_data(j, xvar, cond_name, condition_average, std_err)
         raise RuntimeError('No condition data found for %s'
                            % (self.growth_data.data_files[i].name))
 
@@ -208,13 +198,19 @@ class DataManager():
             xdatas.append(xdata)
             ydatas.append(ydata)
         yerr = None
-        if len(xdatas) > 1:
-            xdata, ydata, yerr = average_data(xdatas, ydatas, std_err)
         # Average condition data over time
-        if(condition_average != -1):
-            # Do something
+        if len(xdatas) > 1 and condition_average != -1:
             xdata, ydata, yerr = \
-                time_average(xdata, ydata, condition_average, std_err)
+                time_average_arrays(xdatas, ydatas, condition_average, std_err)
+        elif len(xdatas) > 1:
+            xdata, ydata, yerr = average_data(xdatas, ydatas, std_err)
+        elif len(xdatas) == 1 and condition_average != -1:
+            xdata, ydata, yerr = \
+                time_average(xdatas[0], ydatas[0], condition_average, std_err)
+        elif len(xdatas) == 1:
+            return xdatas[0], ydatas[0], None
+        else:
+            raise RuntimeError('No condition data found at index %i' % i)
         return xdata, ydata, yerr
 
     def get_condition_ytitle(self, i, yvar=None, yname=None, yunit=None):
@@ -296,7 +292,7 @@ class DataManager():
         averages = []
         errors = []
         for i, _ in enumerate(self.growth_data.data_files):
-            xdata, ydata = self.get_condition_xy_data(i, cond_name)
+            xdata, ydata, _ = self.get_condition_xy_data(i, cond_name)
             dat = np.array([])
             for i, x in enumerate(xdata):
                 if x >= start_t and x <= end_t:
@@ -317,7 +313,7 @@ class DataManager():
         logger.debug('Getting condition %s at time %.2f' % (cond_name, time))
         values = []
         for i, _ in enumerate(self.growth_data.data_files):
-            xdata, ydata = self.get_condition_xy_data(i, cond_name)
+            xdata, ydata, _ = self.get_condition_xy_data(i, cond_name)
             values.append(np.interp(time, xdata, ydata))
         return values
 

ada/data/processor.py
@@ -165,7 +165,7 @@ def time_average(xdata, ydata, window, show_err=False):
     while(i < len(xdata)):
         data_x = np.array([])
         data_y = np.array([])
-        while(i < len(xdata) and xdata[i] < w_i*window):
+        while(i < len(xdata) and xdata[i] < w_i * window):
             data_x = np.append(data_x, xdata[i])
             data_y = np.append(data_y, ydata[i])
             i = i + 1
@@ -180,6 +180,44 @@ def time_average(xdata, ydata, window, show_err=False):
         new_xdata = np.append(new_xdata, mean_x)
         new_ydata = np.append(new_ydata, mean_y)
         if(data_y.size == 1):
+            new_yerr = np.append(new_yerr, 0)
+        else:
+            new_yerr = np.append(new_yerr, std_dev)
+        w_i = w_i + 1
+    return new_xdata, new_ydata, new_yerr
+
+# Function to average arrays of data over time period
+def time_average_arrays(xdatas, ydatas, window, show_err=False):
+    logger.debug('Averaging data over time window of %i' % window)
+    new_xdata = np.array([])
+    new_ydata = np.array([])
+    new_yerr = np.array([])
+    w_i = 1
+    data_remaining = True
+    while data_remaining:
+        window_x = np.array([])
+        window_y = np.array([])
+        finished = True
+        for i, xdata in enumerate(xdatas):
+            # Check if there's any data in the next window
+            if len(xdata[xdata >= (w_i) * window]) > 0:
+                finished = False
+            mask = ((xdata >= (w_i - 1) * window) & (xdata < w_i * window))
+            window_x = np.append(window_x, xdata[mask])
+            window_y = np.append(window_y, ydatas[i][mask])
+        if finished:
+            data_remaining = False
+        if(window_y.size == 0):
+            w_i = w_i + 1
+            continue
+        mean_x = np.mean(window_x)
+        mean_y = np.mean(window_y)
+        std_dev = np.std(window_y, ddof=1)
+        if(show_err):
+            std_dev = std_dev/np.sqrt(window_y.size)
+        new_xdata = np.append(new_xdata, mean_x)
+        new_ydata = np.append(new_ydata, mean_y)
+        if(window_y.size == 1):
             new_yerr = np.append(new_yerr, 0)
         else:
             new_yerr = np.append(new_yerr, std_dev)